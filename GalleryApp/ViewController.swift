624fcd3cc14ec8c6566e993903797b7a02e8c2f6
eojine <eozinee@gmail.com>
2021-01-08 14:58:12 +0900

    Initial Commit

---
+++ GalleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by Eojin Yang on 2021/01/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
