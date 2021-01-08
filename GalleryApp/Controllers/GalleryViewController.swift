707dd069125db4979b8d5886b26d126dff6e3483
eojine <eozinee@gmail.com>
2021-01-08 17:05:43 +0900

    feat: Loading중일 때 loading Cell 띄우기

--- GalleryApp/Controllers/GalleryViewController.swift
+++ GalleryApp/Controllers/GalleryViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 final class GalleryViewController: UIViewController {
-
+    
     @IBOutlet private weak var galleryTableView: UITableView!
     
-    private let defaultHeight: CGFloat = 100.0
+    private var isPaging = false // 현재 페이징중인지 체크
+    private var hasNextPage = false // 다음 페이지를 가지는지 체크
+    private let defaultHeight: CGFloat = 44.0
+    private let defaultCellCount = 0
+    private let loadingCellCount = 1
+    
     let imageHeight: [CGFloat?] = [200, 300, 400]
     
     override func viewDidLoad() {
@@ -28,36 +33,64 @@ final class GalleryViewController: UIViewController {
         galleryTableView.register(cellNibName,
                                   forCellReuseIdentifier: GalleryTableViewCell.identifier)
         galleryTableView.register(loadingNibName,
-                                  forCellReuseIdentifier: GalleryTableViewCell.identifier)
+                                  forCellReuseIdentifier: LoadingTableViewCell.identifier)
     }
     
 }
 
 extension GalleryViewController: UITableViewDataSource {
     
+    enum Section: Int, CaseIterable {
+        case data // section1: 데이터 표시
+        case loading // section2: 로딩셀 표시
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return Section.allCases.count
+    }
+    
     func tableView(_ tableView: UITableView,
                    heightForRowAt indexPath: IndexPath) -> CGFloat {
-        
-           if let height = imageHeight[indexPath.row] {
-               return height
-           }else{
-               return defaultHeight
-           }
-       }
-
+        if let height = imageHeight[indexPath.row], !isPaging {
+            return height
+        }
+        return defaultHeight
+    }
+    
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
-        
-        return imageHeight.count
+        let section = Section(rawValue: section)
+        switch section {
+        case .data:
+            return imageHeight.count
+        case .loading where isPaging && hasNextPage:
+            return loadingCellCount
+        default:
+            return defaultCellCount
+        }
     }
     
     func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: GalleryTableViewCell.identifier,
-                                                       for: indexPath) as? GalleryTableViewCell
-        else { return UITableViewCell() }
-        return cell
+        let defaultCell = UITableViewCell()
+        let section = Section(rawValue: indexPath.section)
+        switch section {
+        case .data:
+            guard let cell = tableView
+                    .dequeueReusableCell(withIdentifier: GalleryTableViewCell.identifier,
+                                         for: indexPath) as? GalleryTableViewCell
+            else { return defaultCell }
+            return cell
+        case .loading:
+            guard let cell = tableView
+                    .dequeueReusableCell(withIdentifier: LoadingTableViewCell.identifier,
+                                         for: indexPath) as? LoadingTableViewCell
+            else { return defaultCell }
+            cell.start()
+            return cell
+        default:
+            return defaultCell
+        }
     }
     
 }
