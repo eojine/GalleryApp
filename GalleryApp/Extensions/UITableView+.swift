c1458ff26688abc0558855e409a473792d6d2ed0
eojine <eozinee@gmail.com>
2021-01-10 16:37:30 +0900

    feat: LoadingCell 삭제하고, TableView Extension에 Bottom Loadin View 생성 함수 추가

---
+++ GalleryApp/Extensions/UITableView+.swift
@@ -0,0 +1,24 @@
+//
+//  UITableView+.swift
+//  GalleryApp
+//
+//  Created by Eojin Yang on 2021/01/10.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func createBottomSpinnerView() {
+        let footerView = UIView(frame: CGRect(x: 0,
+                                              y: 0,
+                                              width: self.frame.size.width,
+                                              height: 44))
+        let spinner = UIActivityIndicatorView()
+        spinner.center = footerView.center
+        footerView.addSubview(spinner)
+        spinner.startAnimating()
+        self.tableFooterView = footerView
+    }
+    
+}
