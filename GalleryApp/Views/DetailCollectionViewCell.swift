c4f18f9e93e27137de66aacbf35d718b20d62232
eojine <eozinee@gmail.com>
2021-01-11 17:17:46 +0900

    feat: DetailCollectionViewCell 추가

--- GalleryApp/Views/DetailCollectionViewCell.swift
+++ GalleryApp/Views/DetailCollectionViewCell.swift
@@ -7,11 +7,20 @@
 
 import UIKit
 
-class DetailCollectionViewCell: UICollectionViewCell {
+final class DetailCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var detailImageView: UIImageView!
+    
+    static let identifier = String(describing: DetailCollectionViewCell.self)
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func configure(title: String) {
+        titleLabel.text = title
     }
 
 }
